35bf1211a59cf0331ac9717632e968f44af06585
Phil Youn <phillipyoun@gmail.com>
2022-05-05 16:16:58 -0700

    Refactorr importing statements and add alert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Button from 'react-bootstrap/Button';
+    /*Refactored to below, easier for syntax but might just import entire library */
+import {Button, Alert} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Alert variant='none'>This is a button</Alert>
         <Button>Test Button</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
